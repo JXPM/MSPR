57a005945d71445ae70a83e0033b98d92926423f
JXPM <bilejohan04@gmail.com>
2026-02-25 14:06:52 +0100

    maj de readme

---
+++ journal.sh
@@ -0,0 +1,16 @@
+# Créer un nouveau dépôt GitHub 
+git init
+git branch -M main
+git add .
+git commit -m "first commit"
+gh repo create MSPR --private
+git remote add origin https://github.com/JXPM/MSPR.git
+git push --set-upstream origin main
+
+
+
+#fichier Maj et push
+git status
+git add image.png journal.sh README.md
+git commit -m "maj de readme"
+git push origin main
